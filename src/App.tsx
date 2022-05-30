ef9531666a84028f87722683257fdf87a8e6e651
AyrDS <ayrtonds97@gmail.com>
2022-05-30 11:50:53 -0300

    Inicio v2

--- src/App.tsx
+++ src/App.tsx
@@ -11,40 +11,53 @@ const promises: string[] = [`https://api.vatcomply.com/rates?base=USD`, 'https:/
 
 const App = () => {
 
-  const [ratesOptions, setRatesOptions] = useState<string[]>([])
+  const [ratesOptions, setRatesOptions] = useState<string[]>([]) //Todas las opciones para los selects
+  const [allCurrencies, setAllCurrencies] = useState() //Todas las monedas
+  const [currentFromCurrency, setCurrentFromCurrency] = useState() //Moneda actual de origen seleccionada
+  const [amount, setAmount] = useState<number>(1) //Cantidad a convertir
+
+  const [fromCurrency, setFromCurrency] = useState<string>('USD'); // Moneda "DE"
+  const [toCurrency, setToCurrency] = useState<string>('EUR'); //Moneda "A"
 
   useEffect(() => {
     Promise.all([fetch(promises[0]), fetch(promises[1])])
       .then(responses => Promise.all(responses.map(response => response.json())))
       .then(data => {
         setRatesOptions(Object.keys(data[0].rates));
+        setAllCurrencies(data[1]);
       })
   }, []);
 
+  const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(Number(e.target.value))
+  }
 
   return (
     <main className="app-container" >
       <div className="coverPage">
-        <p>Convert </p>
+        <p>Convert {amount.toFixed(2)} {allCurrencies?.[fromCurrency]}  </p>
       </div>
       <section className="change-container" >
 
         <form className="change-form" >
           <div className="change-form_labels" >
             <label htmlFor="amount" >Amount</label>
-            <input type="number" id="amount" min={0} />
+            <input type="number" id="amount" min={0} value={amount} onChange={handleAmountChange} />
           </div>
 
           <SelectCurrency
             labelText="From"
             rates={ratesOptions}
-            valueSelect="EUR"
+            valueSelect={fromCurrency}
             onChangeCurrency={() => { }}
           />
 
-          {/* <SelectCurrency
+          <SelectCurrency
             labelText="To"
-          /> */}
+            rates={ratesOptions}
+            valueSelect={toCurrency}
+            onChangeCurrency={() => { }}
+          />
 
         </form>
 
